040d8a4b5e1faf48874450edc6eae45af7c60eeb
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 18:53:31 -0800

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,12 +1,12 @@
 import React, { createContext, useState } from "react";
 import all_product from "../Components/Asset/all_product";
 
-//create shopcontext using createcontext
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < all_product.length + 1; index++) {
+  for (let index = 0; index < all_product.length + 1; index++) 
+  {
     cart[index] = 0;
   }
   return cart;
@@ -30,8 +30,10 @@ const contextValue = {
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
-    for (const item in cartItems) {
-      if (cartItems[item] > 0) {
+    for (const item in cartItems) 
+    {
+      if (cartItems[item] > 0) 
+      {
         let itemInfo = all_product.find(
           (product) => product.id === Number(item)
         );
@@ -47,8 +49,10 @@ const contextValue = {
   
   const getTotalCartItems = () => {
     let totalItem = 0;
-    for (const item in cartItems) {
-      if (cartItems[item] > 0) {
+    for (const item in cartItems) 
+    {
+      if (cartItems[item] > 0) 
+      {
         totalItem += cartItems[item];
       }
     }
